21a5ca7b235ca2705da31c888fb6de43a551accb
Jaylin Gross <grossjaylin@gmail.com>
2021-08-31 14:19:33 -0400

    second commit

--- script.js
+++ script.js
@@ -1,54 +1,57 @@
-$(document).ready(function (){
+
+var todayDate = moment().format('August 31st 2021, 2:18:45 pm');
+$("#currentDay").html(todayDate);
+
+$(document).ready(function () {
     
-    $("currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-
     $(".saveBtn").on("click", function () {
-
-        console.log(this);
-        var text = (this).siblings(".description").val();
+       
+        var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
+        
         localStorage.setItem(time, text);
     })
-
-$("#hour8 .description").val(localStorage.getItem("hour8"));
-$("#hour9 .description").val(localStorage.getItem("hour9"));
-$("#hour10 .description").val(localStorage.getItem("hour10"));
-$("#hour11 .description").val(localStorage.getItem("hour11"));
-$("#hour12 .description").val(localStorage.getItem("hour12"));
-$("#hour13 .description").val(localStorage.getItem("hour13"));
-$("#hour14 .description").val(localStorage.getItem("hour14"));
-$("#hour15 .description").val(localStorage.getItem("hour15"));
-$("#hour16 .description").val(localStorage.getItem("hour16"));
-$("#hour17 .description").val(localStorage.getItem("hour17"));
-
-
-function hourTracker() {
-    
-    var currentHour = moment().hour(); 
-
-    
-    $(".time-block").each(function () {
-        var blockHour = parseInt($(this).attr("id").split("hour")[1]);
-        console.log( blockHour, currentHour)
+   
+    function timeTracker() {
+       
+        var timeNow = moment().hour();
 
         
-        if (blockHour < currentHour) {
-            $(this).addClass("past");
-            $(this).removeClass("future");
-            $(this).removeClass("present");
-        }
-        else if (blockHour === currentHour) {
-            $(this).removeClass("past");
-            $(this).addClass("present");
-            $(this).removeClass("future");
-        }
-        else {
-            $(this).removeClass("present");
-            $(this).removeClass("past");
-            $(this).addClass("future");
-        }
-    })
-}
-hourTracker(); 
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+            }
+        })
+    }
+
+    
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+    timeTracker();
 })
